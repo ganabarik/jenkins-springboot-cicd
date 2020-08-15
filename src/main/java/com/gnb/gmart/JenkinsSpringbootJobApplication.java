dbad40ed5864539c128eac421a6e4a63fa740983
gananath <ganesh.jpa4@gmail.com>
2020-08-15 12:57:49 +0530

    initial commit

---
+++ src/main/java/com/gnb/gmart/JenkinsSpringbootJobApplication.java
@@ -0,0 +1,13 @@
+package com.gnb.gmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringbootJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringbootJobApplication.class, args);
+	}
+
+}
