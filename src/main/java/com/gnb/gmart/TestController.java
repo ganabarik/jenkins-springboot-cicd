075ad689ea4e0e605c4d7b01147d6a59f7a1e51e
gananath <ganesh.jpa4@gmail.com>
2020-08-15 13:14:45 +0530

    modified

--- src/main/java/com/gnb/gmart/TestController.java
+++ src/main/java/com/gnb/gmart/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 	
 	@GetMapping("/message")
 	public String getMessage() {
-		return "welcome to jenkins pipeline-CI/CD";
+		return "welcome to jenkins pipeline-CI/CD/Docker";
 	}
 
 }
